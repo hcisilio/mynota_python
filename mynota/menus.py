31407be81ed7c84e8421c12105a9e4983ccd715b
Helber Cisilio <helber.santos@iff.edu.br>
2016-04-18 11:40:21 -0300

    Finalização do menu v1.0

--- mynota/menus.py
+++ mynota/menus.py
@@ -2,6 +2,20 @@
 from django.core.urlresolvers import reverse
 from menu import Menu, MenuItem
 
+#submenus
+aula_children = (
+    MenuItem("Registrar aula ministrada",
+             "/aula/add/",
+             weight=10,
+             icon="aula",
+             check=lambda request: request.user.has_perm('mynota.add_aula')),
+    MenuItem("Registrar plano de aula",
+             "/plano_aula/add/",
+             weight=10,
+             icon="aula",
+             check=lambda request: request.user.has_perm('mynota.add_planoaula')),
+)
+
 #itens do menu
 Menu.add_item("main", MenuItem("Home",
                                "/",
@@ -16,30 +30,36 @@ Menu.add_item("main", MenuItem("Aluno",
 Menu.add_item("main", MenuItem("Professor",
                                "/admin/mynota/professor/",
                                weight=10,
-                               icon="home",
+                               icon="professor",
 							   check=lambda request: request.user.has_perm('mynota.change_professor')))
 Menu.add_item("main", MenuItem("Curso",
                                "/admin/mynota/curso/",
                                weight=10,
-                               icon="home",
+                               icon="curso",
 							   check=lambda request: request.user.has_perm('mynota.change_curso')))
 Menu.add_item("main", MenuItem("Turma",
                                "/admin/mynota/turma/",
                                weight=10,
-                               icon="home",
+                               icon="turma",
 							   check=lambda request: request.user.has_perm('mynota.change_turma')))
+# Menu.add_item("main", MenuItem("Aulas",
+#                                ".",
+#                                weight=10,
+#                                icon="aula",
+#                                children=aula_children,
+# 							   check=lambda request: request.user.has_perm('mynota.add_aula')))
 Menu.add_item("main", MenuItem("Aulas",
                                "/aula/add/",
                                weight=10,
-                               icon="home",
+                               icon="aula",
 							   check=lambda request: request.user.has_perm('mynota.add_aula')))
 Menu.add_item("main", MenuItem("Planos de Aula",
                                "/plano_aula/add/",
                                weight=10,
-                               icon="home",
+                               icon="aula",
 							   check=lambda request: request.user.has_perm('mynota.add_planoaula')))
 Menu.add_item("main", MenuItem("Notas",
                                "/notas/listar/",
                                weight=10,
-                               icon="home",
+                               icon="nota",
 							   check=lambda request: request.user.has_perm('mynota.add_nota')))
