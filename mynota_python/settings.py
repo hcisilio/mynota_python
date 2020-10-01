b284694a798efd68a1a2b8ed6db40ae5e1dcc44d
Helber Cisilio <hcisilio@users.noreply.github.com>
2020-10-01 12:41:23 -0300

    Update settings.py

--- mynota_python/settings.py
+++ mynota_python/settings.py
@@ -8,3 +8,14 @@ except ImportError as e:
 import django_heroku
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
